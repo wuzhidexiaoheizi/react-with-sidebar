e2ba7e8aa2b6000f7c2dd4d79c274c278f59ef7f
殷众 <yinz@uinz.local>
2016-01-15 19:58:11 +0800

    use react-addons-update

--- app/reducers/detail.js
+++ app/reducers/detail.js
@@ -1,22 +1,24 @@
+import update from 'react-addons-update';
 import {parseData} from '../helper';
 
 const initialState = {};
 
 export default function(state = initialState, action) {
-  let _item;
   switch (action.type) {
   case 'FETCH_DETAIL_DONE':
-    return Object.assign({}, state, {[action.id]: parseData(action.item, action.td)});
+    return update(state, {
+      [action.id]: {$set: parseData(action.item, action.td)}
+    });
 
   case 'ITEM_STARTED':
-    _item = Object.assign({}, state[action.id]);
-    _item.status = 'started';
-    return Object.assign({}, state, {[action.id]: _item});
+    return update(state, {
+      [action.id]: {status: {$set: 'started'}}
+    });
 
   case 'ITEM_END':
-    _item = Object.assign({}, state[action.id]);
-    _item.status = 'end';
-    return Object.assign({}, state, {[action.id]: _item});
+    return update(state, {
+      [action.id]: {status: {$set: 'end'}}
+    });
 
   default: return state;
   }
