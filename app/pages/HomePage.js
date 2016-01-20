e70a781102bb2701a2b8ea95d3c0c12797c581fd
zhangrui <517144093@qq.com>
2016-01-20 12:46:23 +0800

    修改lint配置文件

--- app/pages/HomePage.js
+++ app/pages/HomePage.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import config from '../config.json'; 
+import config from '../config.json';
 
 class HomePage extends Component {
   constructor(props) {
