594ed70994baab7b40e040ce7d8fd1a593a8933d
zhangrui <517144093@qq.com>
2016-07-26 17:37:52 +0800

    背景音乐循环播放

--- app/components/MusicPlayer.js
+++ app/components/MusicPlayer.js
@@ -1,30 +1,30 @@
 import React, { Component } from 'react';
 
-export default class MusicPlayer extends Component{
+export default class MusicPlayer extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      rotate : 'rotate',
+      rotate: 'rotate',
       play: true
     };
   }
 
-  rotatePlayer(){
-    const status = this.props.status;
-    this.props.onRotate();
-    
-    status ? this.refs.audio.pause() : this.refs.audio.play(); 
-    
+  rotatePlayer() {
+    const { status, onRotate } = this.props;
+
+    if (typeof onRotate == 'function') onRotate();
+
+    status ? this.refs.audio.pause() : this.refs.audio.play();
   }
 
-  render(){
+  render() {
     const status = this.props.status;
     status ? this.state.rotate = 'rotate' : this.state.rotate = '';
-    
+
     return (
-        <section className='player-container' onClick={this.rotatePlayer.bind(this)}>
-          <audio src={this.props.resource} autoPlay='autoplay' ref='audio'>your browser doesn't support</audio>
+        <section className="player-container" onClick={this.rotatePlayer.bind(this)}>
+          <audio src={this.props.resource} autoPlay="autoplay" ref="audio" loop="loop">your browser doesn't support</audio>
           <div className={`music-player ${this.state.rotate}`}>
           </div>
         </section>
